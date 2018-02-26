b8908ddac37fe354d03a6fe559b1652a72c59426
Aashish Tiwari <aashishtiwari97@gmail.com>
2018-02-27 00:02:09 +0530

    Updated React native folder

--- React-native/App.js
+++ React-native/App.js
@@ -1,6 +1,6 @@
 
 import React, { Component } from 'react';
-import { StyleSheet, View, Image, Platform, TouchableHighlight,Dimensions, ScrollView,FlatList,ListView } from 'react-native';
+import { StyleSheet, View, Image, Platform, TouchableHighlight,Dimensions, ScrollView,FlatList,ListView, WebView,AppState} from 'react-native';
 import{Content ,Icon,Container,Button,Header,Drawer,Thumbnail,Footer,Item,Input,Text,Left} from 'native-base';
 import { DrawerNavigator,StackNavigator,TabNavigator } from 'react-navigation';
 import ActionButton from 'react-native-action-button';
@@ -8,7 +8,8 @@ import ActionButton from 'react-native-action-button';
 import InvertibleScrollView from 'react-native-invertible-scroll-view';
 
 import SideBar from './src/Components/SideBar.js';
-import TopChartsScreen from './src/Components/TopCharts.js';
+import TopCharts from './src/Components/TopCharts.js';
+
 
  class App extends Component {
 
@@ -18,26 +19,31 @@ import TopChartsScreen from './src/Components/TopCharts.js';
     super(props);
 
     this.state = {
-
-      chatHistory:[],
-      input: '',
-      context:'',
-      UserInput:[],
-      showInput:'',
-
+      appState: AppState.currentState,
+      chatHistory :   [],
+      input       :   '',
+      context     :   '',
+      UserInput   :   [],
+      showInput   :   '',
+      toggle      :   false,
+      type        :   'user',
+      VideoData   :   [],
+      youtubeId     :   '',
         };
+
 
 
   }
 
 
-//fetching bot's greeting message
-componentDidMount() {
 
-      var inp = 'artist jagjit singh'
+//fetching bot's greeting message
+componentWillMount() {
+
+      var inp = ''
       var vcontext = {}
 
-  fetch('https://my-app.cession48.hasura-app.io/api/message',{
+  fetch('https://app.amenable52.hasura-app.io/api/message',{
     method: 'POST',
     body: JSON.stringify({ input : {text: inp}, context : vcontext} ),
     headers: { 'Content-Type':'application/json;charset=UTF-8' }
@@ -46,59 +52,94 @@ componentDidMount() {
   .then( data => {
     console.log("first");
     console.log(data);
-
     let mes=data.output.text[0];
     let con=data.context;
+
 
     console.log("recieved context" )
     console.log(con);
     this.setState({chatHistory: [
         {
-          type: 'bot',
+          type: 'BOT : ',
           message: mes,
         }
       ], context: con});
+
+
   })
   .catch((error) => {
-      alert(error);
+      console.log(error);
       });
 
-}
+  }
 
 
-onMessageSend(){
-var ms = this.state.input;
- var obj= { type: 'user', message: ms};
-this.setState({ChatHistory: this.state.chatHistory.concat(obj),UserInput:this.state.UserInput.concat(obj),showInput:ms,input:''});
+  onMessageSend(){
+    let messageTextStyle;
+    let mes;
 
-    var inp = ms
-    var vcontext = this.state.context
+  var ms = this.state.input;
+   var obj= { type: 'USER : ', message: ms};
+  this.setState({ChatHistory: this.state.chatHistory.push(obj),showInput:ms,input:''});
 
- fetch('https://my-app.cession48.hasura-app.io/api/message',{
-   method: 'POST',
- body: JSON.stringify({ input : {text: inp}, context : vcontext} ),
-  headers: { 'Content-Type':'application/json; charset=UTF-8' }
- })
- .then(results => results.json())
- .then( data => {
-   let mes=data.output.text[0];
-   let con=data.context;
-var newObj = {
-   type: 'bot',
-   message: mes,
- }
-   this.setState({
-     chatHistory: this.state.chatHistory.concat(newObj),
-     context: con});
- })
+      var Video = [];
+      var inp = ms
+      var vcontext = this.state.context
 
-}
+   fetch('https://app.amenable52.hasura-app.io/api/message',
+    { method: 'POST',
+   body: JSON.stringify({ input : {text: inp}, context : vcontext} ),
+    headers: { 'Content-Type':'application/json; charset=UTF-8' }
+   })
+   .then(results => results.json())
+   .then( data => {
+     mes=data.output.text[0];
+     let con=data.context;
+     console.log(mes);
+     var newObj = {
+         type: 'BOT : ',
+         message: mes,
+   }
+     this.setState({
+       chatHistory: this.state.chatHistory.concat(newObj),
+       context: con});
+         if(mes ==="Sure. Will display results soon")
+         {
 
+           return fetch('https://app.amenable52.hasura-app.io/api/playmusic', {
+             method: 'POST',
+             body: JSON.stringify({
+                q: ms
+             }),
+             headers: {
+               "Content-Type": "application/json; charset=UTF-8"
+             }
+         })
+         .then(results => results.json())
+         .then(responseJson => {
+           Video.push(responseJson);
 
-//bot message render function.
+           let thumbnail = responseJson[0].thumbnailurl;
+           let videoId = responseJson[0].videoid;
+           let URl = responseJson[0].url;
 
+           var VideoObj = {
+              type: 'BOT : ',
+              Url : URl,
+              ThumbnailUrl:thumbnail,
+              videoID:videoId,
+            }
+            this.setState({
+              chatHistory: this.state.chatHistory.concat(VideoObj),
+              context: con});
+              })
+       }
+   })
+
+  }
 
   render() {
+    const {navigate} = this.props.navigation;
 
     const window = Dimensions.get('window')
     const imageDimensions = {
@@ -113,41 +154,30 @@ var newObj = {
         <Content style={{flex: 1}}>
 
         <ScrollView style={styles.container}>
-        <View>
-      {this.state.chatHistory.map(( chat,index) =>
-        <View  style={styles.leftBubble}>
-              <Left>
-                <Thumbnail small source={{ uri: 'https://thumb1.shutterstock.com/display_pic_with_logo/2826565/737510584/stock-vector-chatbot-icon-cute-robot-working-behind-laptop-modern-bot-sign-design-smiling-customer-service-737510584.jpg' }} />
-              </Left>
-                <View style={styles.leftBubbleText}>
-                   < Text key={index}style={styles.leftBubbleTextStyle}>{alert(chat.message)}</Text>
-                </View>
 
-              <View style={styles.leftBubbleTime}>
-                <Text style={styles.leftBubbleTimeStyle}>16:33</Text>
-              </View>
-        </View>)}
-{this.state.UserInput.map(( input,index) =>
-            <View  style={styles.rightBubble}>
+            <View >{this.state.chatHistory.map(( chat,index) =>
+                      <View  style={chat.type === 'BOT : ' ? styles.leftBubble : styles.rightBubble}>
+                            <Left>
+                              <Thumbnail  small source={{  uri: chat.type === 'BOT : ' ? 'https://thumb1.shutterstock.com/display_pic_with_logo/2826565/737510584/stock-vector-chatbot-icon-cute-robot-working-behind-laptop-modern-bot-sign-design-smiling-customer-service-737510584.jpg':'https://cdn1.iconfinder.com/data/icons/mix-color-4/502/Untitled-1-512.png' }} />
+                            </Left>
 
-                <View style={styles.leftBubbleText}>
-                 < Text key={index}style={styles.leftBubbleTextStyle}>{input.message}</Text>
-                </View>
+                            <View style={chat.type === 'BOT : ' ? styles.leftBubble : styles.rightBubble}>
+                               < Text key={index} style={chat.type === 'BOT : ' ? styles.leftBubbleTextStyle : styles.rightBubbleTextStyle  }>{[chat.message]}</Text>
 
-              <View style={styles.leftBubbleTime}>
-                <Text style={styles.leftBubbleTimeStyle}>16:33</Text>
-              </View>
-        </View> )}
-
-        </View>
+                               <TouchableHighlight style={{flex:4}} key={chat.videoId}onPress={()=> this.props.navigation.navigate('TopCharts',{youtubeId: chat.videoID})}>
+                               <View>
+                                    <Image source={{uri: chat.ThumbnailUrl}} style={{width: 320, height: 180}}></Image>
+                               </View>
+                               </TouchableHighlight>
+                            </View>
+                      </View>)}
+            </View>
         </ScrollView>
       </Content>
       <Footer>
               <Item style={{flex: 1,backgroundColor:'#fff'}}>
                   <Input
                   placeholder='Type your message here'
-
-
                   onChangeText={(text) => {
                     this.setState({
                       input:text,
@@ -164,64 +194,75 @@ var newObj = {
       </Footer>
       </Container>
     );
+
   }
 }
-
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    padding: 16,
+    padding: 4,
+
   },
   leftBubble: {
-      flex: 1,
+      flex:4,
       flexDirection: 'row',
-      backgroundColor: '#cef442',
 
+      justifyContent: 'center',
       borderRadius: 4,
       minWidth: 66,
-      maxWidth: 288,
+      maxWidth: 320,
       marginBottom: 8
   },
   rightBubble: {
-      flex: 1,
-      flexDirection: 'row',
-      backgroundColor: '#a4c2f2',
 
+      flexDirection: 'row',
+      flex:4,
+      marginBottom: 8,
       borderRadius: 4,
+      marginLeft:4,
+      marginRight:4,
       minWidth: 66,
-      maxWidth: 288,
-      justifyContent: 'flex-end',
-      paddingLeft:0,
+      maxWidth: 320,
+      justifyContent: 'center',
+
   },
   leftBubbleText:{
-    flex:0,
+    flex:3,
+    backgroundColor: '#271dd6',
     maxWidth: 247,
+    borderRadius: 4,
     padding: 8,
     paddingLeft: 12,
     paddingRight:0,
   },
+
   leftBubbleTextStyle:{
     fontFamily: 'WorkSans-Regular',
+    backgroundColor: '#6f68ed',
+    borderRadius: 8,
     fontSize: 14,
-    color: '#000000',
+    color: '#fff',
     lineHeight: 18,
   },
-  leftBubbleTime:{
-    flex:1,
-    flexDirection: 'row',
-    alignItems: 'flex-end',
-    paddingRight: 3,
-    paddingBottom: 2,
-    marginLeft: 5,
-  },
-  leftBubbleTimeStyle:{
-    fontFamily: 'WorkSans-Regular',
-    fontSize: 10,
-    color: 'rgba(0,0,0,0.54)',
-    lineHeight: 16,
 
+  rightBubbleTextStyle:{
+    fontFamily: 'WorkSans-Regular',
+    backgroundColor: '#067a7a',
+    borderRadius: 8,
+    marginLeft:4,
+    marginRight:4,
+    fontSize: 14,
+    color: '#fff',
+    lineHeight: 18,
   },
+  button: {
+    marginBottom: 30,
+    width: 260,
+    alignItems: 'center',
+    backgroundColor: '#2196F3'
+  },
+
 });
 //DrawerButton
 const DrawerButton=({navigation})=>(
@@ -235,12 +276,11 @@ const Stack = StackNavigator({
                       Listen: {
                         screen:App,
                         navigationOptions:({navigation}) =>({
-                          title:'SongBot',
+                          title:'MusicBot',
                           headerLeft: <DrawerButton navigation={navigation} />,
                           headerStyle: {
                                               backgroundColor: '#fff',
                                               elevation:0,
-
                                         },
                         }),
                       },
@@ -253,16 +293,9 @@ const Stack = StackNavigator({
                     Listen:{
                       screen:Stack,
                     },
-
-                          TopCharts: {
-                            screen: TopChartsScreen,
-
-                          },
-
-
-
-
-
+                    TopCharts: {
+                      screen: TopCharts,
+                    },
                   },
                       {
                         drawerOpenRoute: 'DrawerOpen',
--- React-native/src/Components/TopCharts.js
+++ React-native/src/Components/TopCharts.js
@@ -1,7 +1,20 @@
 import React, { Component } from 'react';
 import { Container, Header, Content, List,Title, ListItem, Text,Icon,Left,Button ,Body} from 'native-base';
-export default class TopChartsScreen extends Component {
+
+import { StyleSheet, View, WebView, Platform } from 'react-native';
+import {StackNavigator} from 'react-navigation';
+
+export default class TopCharts extends Component {
+
+
+  constructor(props){
+    super(props);
+
+  }
+
   render() {
+    const {state} = this.props.navigation;
+
     return (
       <Container>
       <Header  >
@@ -12,32 +25,33 @@ export default class TopChartsScreen extends Component {
           </Button>
         </Left>
         <Body>
-          <Title>List</Title>
+          <Title>Youtube Viewer</Title>
         </Body>
         </Header>
-        <Content>
-          <List>
-            <ListItem>
-              <Text>COMEDY</Text>
-            </ListItem>
-            <ListItem >
-              <Text>Hangover</Text>
-            </ListItem>
-            <ListItem>
-              <Text>Horrible Bosses</Text>
-            </ListItem>
-            <ListItem>
-              <Text>Conjuring</Text>
-            </ListItem>
-            <ListItem >
-              <Text>ACTION</Text>
-            </ListItem>
-            <ListItem>
-              <Text>Terminator Genesis</Text>
-            </ListItem>
-          </List>
-        </Content>
+        <View style={{ height: 300 }}>
+
+           <WebView
+                   style={ styles.WebViewContainer }
+                   javaScriptEnabled={true}
+
+                    automaticallyAdjustContentInsets={false}
+                   source={{uri: 'https://www.youtube.com/embed/'+`${state.params.youtubeId}` }}
+           />
+
+       </View>
       </Container>
     );
   }
 }
+
+
+
+const styles = StyleSheet.create({
+
+WebViewContainer: {
+    flex:1,
+    marginTop: (Platform.OS == 'ios') ? 20 : 0,
+
+  }
+
+});
